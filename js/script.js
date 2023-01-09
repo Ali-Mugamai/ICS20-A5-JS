156e8b4a0e3d81e0d1c01f8801bc38f702c51df0
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-08 23:02:58 -0500

    mhvnfc

--- js/script.js
+++ js/script.js
@@ -14,27 +14,27 @@ if (navigator.serviceWorker) {
   })
 }
 
-const n1 = parseInt(document.getElementById("num_1").value)
-const n2 = parseInt(document.getElementById("num_2").value)
-let lcm = 0
-
-if (n1 > n2) {
-  let bigger_num = n1
-  let smaller_num = n2
-  while (bigger_num % smaller_num !== 0) {
-    lcm = lcm + bigger_num
-  }
-} else if (n1 == n2) {
-  lcm = lcm + n1
-} else {
-  let bigger_num = n2
-  let smaller_num = n1
-  while (bigger_num % smaller_num !== 0) {
-    lcm = lcm + bigger_num
-  }
-}
 function calculate() {
   // output
+  const n1 = parseInt(document.getElementById("num_1").value)
+  const n2 = parseInt(document.getElementById("num_2").value)
+  let lcm = 0
+
+  if (n1 > n2) {
+    let bigger_num = n1
+    let smaller_num = n2
+    while (bigger_num % smaller_num !== 0) {
+      lcm = lcm + bigger_num
+    }
+  }   else if (n1 == n2) {
+    lcm = lcm + n1
+  }   else {
+    let bigger_num = n2
+    let smaller_num = n1
+    while (bigger_num % smaller_num !== 0) {
+      lcm = lcm + bigger_num
+    }
+  }
   document.getElementById("answer").innerHTML =
     "the lowest common multiple is:" + lcm + " ."
 }
