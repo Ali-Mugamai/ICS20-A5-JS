08483b52a9104363ae247c773e67f72d312c9a85
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2023-01-08 19:53:24 -0500

    dsfsfcsac

--- js/script.js
+++ js/script.js
@@ -1,23 +1,29 @@
-// Copyright (c) 2020 ali-mugamai All rights reserved
+// Copyright (c) 2020 Ali Mugamai All rights reserved
 //
-// Created by: ali mugamai
-// Created on: Sep 2020
+// Created by: Ali Mugamai
+// Created on: oct 2022
 // This file contains the JS functions for index.html
 
 "use strict"
-
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS20-A5-JS/sw.js", {
+    scope: "/ICS20-A5-JS/",
   })
 }
-
 /**
- * This function displays an alert.
+ * This function calculates area and perimeter of rectangle.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+
+function calculate() {
+  // input
+  let i = lar;
+  while(i % small !== 0){
+    i += lar;
+  }
+ gyugyguj
+  // output
+  document.getElementById("pay").innerHTML = "<p> the lowest common factor is: </p>" + lcm
 }
